4756c310e50da0b66df8c0cc2b6dfe544da69e16
Srishti-aggarwal <82658521+Srishti-aggarwal@users.noreply.github.com>
2021-06-11 04:01:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+var today=new Date();
+   var principal = document.getElementById("principal").value;
+   var rate=document.getElementById("rate").value;
+   var years=document.getElementById("years").value;
+   var intt=(principal*rate*years)/100;
+   var yy=today.getFullYear();
+   p1.innerHTML= principal;
+   r1.innerHTML= rate;
+   t1.innerHTML= Number(yy)+Number(years);
+   a1.innerHTML= intt;
 }
+
+
         
